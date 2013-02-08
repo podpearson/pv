ff2d7bf6e04f6b8d24cfcdd5740e94a01a00c777
podpearson <rpearson@well.ox.ac.uk>
2013-02-08 14:39:54 +0000

    cleanup

--- R/loadAndGenotypePvVcf.R
+++ R/loadAndGenotypePvVcf.R
@@ -9,26 +9,26 @@
 
 loadAndGenotypePvVcf <- function(
   originalVcfFilename         = "data/genotypes/pv_02.vcf.gz",
-  cleanedAndGenotypedVcf      = sub("\\.vcf\\.gz", "\\.cleanedAndGenotyped\\.vcf", originalVcfFilename),
-  rdaFilename                 = sub("\\.gz", "\\.cleanedAndGenotyped\\.vcf\\.rda", originalVcfFilename),
-  typableRdaFilename          = sub("\\.gz", "\\.typable\\.rda", originalVcfFilename),
+  typableVcf                  = sub("\\.vcf\\.gz", "\\.typable\\.vcf", originalVcfFilename),
+#  rdaFilename                 = sub("\\.gz", "\\.cleanedAndGenotyped\\.vcf\\.rda", originalVcfFilename),
+  typableRdaFilename          = paste(typableVcf, "rda", sep="."),
   reload                      = TRUE
 ) {
   if(!reload && file.exists(typableRdaFilename)) {
     load(typableRdaFilename)
   } else {
-    if(!reload && file.exists(rdaFilename)) {
-      load(rdaFilename)
-    } else {
-      if(reload || !file.exists(cleanedAndGenotypedVcf)) {
-        cleanVcfCommand <- paste("zcat", originalVcfFilename, "| scripts/perl/cleanAndGenotypeMagnusVcf.pl >", cleanedAndGenotypedVcf)
-        system(cleanVcfCommand)
-      }
-      vcf <- readVcf(cleanedAndGenotypedVcf, genome="P. vivax reference, PlasmoDB V6.0")
-      save(vcf, file=rdaFilename)
+#    if(!reload && file.exists(rdaFilename)) {
+#      load(rdaFilename)
+#    } else {
+    if(reload || !file.exists(cleanedAndGenotypedVcf)) {
+      cleanVcfCommand <- paste("zcat", originalVcfFilename, "| scripts/perl/cleanAndGenotypeMagnusVcf.pl >", cleanedAndGenotypedVcf)
+      system(cleanVcfCommand)
     }
-    typableVcf <- vcf[values(info(vcf))[["TYP"]]==TRUE]
+    typableVcf <- readVcf(typableVcf, genome="P. vivax reference, PlasmoDB V6.0")
     save(typableVcf, file=typableRdaFilename)
+#    }
+#    typableVcf <- vcf[values(info(vcf))[["TYP"]]==TRUE]
+#    save(typableVcf, file=typableRdaFilename)
   }
   return(typableVcf)
 }
